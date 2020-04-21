5d2de2ef2a2f476a83d98fe3f6d51999f001d397
SimonGaufreteau <simon.gaufreteau@gmail.com>
2020-04-21 23:07:54 +0200

    Implementation of a GraphParser, which returns a graph from a .col file

---
+++ src/com/graphParser/GraphParser.java
@@ -0,0 +1,68 @@
+package com.graphParser;
+
+import com.graph.Edge;
+import com.graph.Graph;
+import com.graph.Vertex;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class GraphParser {
+
+	private GraphParser(){}
+
+	public static Graph getGraphFromFile(String filepath) throws Exception {
+		File f = new File(filepath);
+		BufferedReader br = new BufferedReader(new FileReader(f));
+
+		String name=f.getName();
+		boolean isDirected=false;
+		int nbVertices=0;
+		int nbEdges=0;
+		int nbVertexValues=0;
+		int nbEdgesValues=0;
+		ArrayList<Vertex> listVertices=new ArrayList<>();
+		ArrayList<Edge> listEdges = new ArrayList<>();
+		ArrayList<LinkedList<Integer>> listAdjacent=new ArrayList<>();
+
+		String[] splitLine;
+		String line = br.readLine();
+		int edgeCounter=0;
+		while (line!=null){
+			switch(line.charAt(0)){
+				case 'c':
+					line=br.readLine();
+					continue;
+				case 'p' :
+					splitLine = line.split(" ");
+					if(splitLine.length<3) throw new Exception("Incorrect line. Split length of the 'p' line must be 3");
+					nbVertices=Integer.parseInt(splitLine[2]);
+					nbEdges=Integer.parseInt(splitLine[3]);
+					for(int i=0;i<nbVertices;i++){
+						Vertex tempVertex = new Vertex(i,"",nbVertexValues,null);
+						listVertices.add(tempVertex);
+						listAdjacent.add( new LinkedList<>());
+					}
+					break;
+				case 'e':
+					splitLine = line.split(" ");
+					if(splitLine.length!=3) throw new Exception("Incorrect line. Split length of the 'e' line must be 3");
+					int initialID= Integer.parseInt(splitLine[1])-1;
+					int finalID=Integer.parseInt(splitLine[2])-1;
+					Edge edge = new Edge(edgeCounter,initialID,finalID,"","",nbEdgesValues,null);
+					listEdges.add(edge);
+					listVertices.get(initialID).incrementDegree();
+					listVertices.get(finalID).incrementDegree();
+					listAdjacent.get(initialID).add(edge.getId());
+					listAdjacent.get(finalID).add(edge.getId());
+					edgeCounter ++;
+					break;
+
+			}
+			line=br.readLine();
+
+		}
+		return new Graph(name,isDirected,nbVertices,nbEdges,nbVertexValues,nbEdgesValues,listVertices,listEdges,listAdjacent);
+	}
+}
